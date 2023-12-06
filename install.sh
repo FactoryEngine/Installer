aed76548c6eed1198fe1f273aefb71cd2497dbdc
Joe Furfaro <joefurf@outlook.com>
2023-12-05 21:27:21 -0500

    Update install to be smarter and use tmp

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+set -e
+
 # Clone the Factory Engine repository
-git clone git@github.com:FactoryEngine/FactoryEngine.git && \
+rm -rf /tmp/FactoryEngine
+git clone git@github.com:FactoryEngine/FactoryEngine.git /tmp/FactoryEngine
 
-# Enter Factory Engine directory
-cd FactoryEngine && \
-
-# Set up Factory Engine CLI
-cd cli && ./install_fe.sh clean && ./install_fe.sh && \
-
-# Build and set up FactoryEngine
-cd ../core && ./setup.sh && \
+# Install the FE CLI
+mkdir -p ~/.fedata/cli
+cp -r --remove-destination /tmp/FactoryEngine/cli ~/.fedata/
+bash ~/.fedata/cli/install_fe.sh clean
+bash ~/.fedata/cli/install_fe.sh
 
 # Install FactoryEngine core
-fe core install . && \
-
-# Delete installer
-cd ../../ && rm install.sh
+fe core install /tmp/FactoryEngine/core --ignore-existing --copy
