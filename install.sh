4899b7dc1d952d0d847bd68708de1ea291f45dc7
Joe Furfaro <joefurf@outlook.com>
2023-12-05 20:32:47 -0500

    Add install.sh for testing

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clone the Factory Engine repository
+git clone git@github.com:FactoryEngine/FactoryEngine.git && \
+
+# Enter Factory Engine directory
+cd FactoryEngine && \
+
+# Set up Factory Engine CLI
+cd cli && ./install_fe.sh clean && ./install_fe.sh && \
+
+# Build and set up FactoryEngine
+cd ../core && ./setup.sh && \
+
+# Install FactoryEngine core
+fe core install . && \
+
+# Delete installer
+cd ../../ && rm installer.sh
